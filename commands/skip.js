5e00acacced8e88d2e81e976ee1c11e6f2810aef
fruithut <fruithut@tutanota.com>
2019-12-23 03:55:49 +0100

    Corrected error-respone

--- commands/skip.js
+++ commands/skip.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'skip',
     description: 'Skip music for discord-bot',
     execute(message, serverQueue) {
-        if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
+        if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to skip the music!');
         if (!serverQueue) return message.channel.send('There are no songs that could be skipped!');
 
         serverQueue.connection.dispatcher.end();
